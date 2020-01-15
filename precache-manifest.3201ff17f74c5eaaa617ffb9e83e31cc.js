80d0360539f452ea65f6f2beda60fbcda3f04e5d
ashir hussain <hussainashir87@gmail.com>
2020-01-15 15:29:29 +0500

    initial commit

---
+++ precache-manifest.3201ff17f74c5eaaa617ffb9e83e31cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd42b1111778d327bf80f06f361f8ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1248bad7302c0f4e7596",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9ef586ebc26bcc836af",
+    "url": "/static/js/2.ecd25bdd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ecd25bdd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1248bad7302c0f4e7596",
+    "url": "/static/js/main.bd5ae6e7.chunk.js"
+  },
+  {
+    "revision": "2bdb5edae6e7f0595dfe",
+    "url": "/static/js/runtime-main.a7998b86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
